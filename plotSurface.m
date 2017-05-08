471c8f81c1afef905f509eb03a9a755d543e1a6d
JimDaggerthuggert <tpoulsen188@gmail.com>
2017-05-08 14:24:59 +0200

    Add files via upload

--- plotSurface.m
+++ plotSurface.m
@@ -1,21 +1,27 @@
-function [] = plotSurface(data, s)
-% Plot field component or magnitude in a plane
-% input: 
-%   An array where row 1 is coordinates in one dimension, and row 2 is
-%   coordinates in another dimension. Row 3 is the value of a field component
-%   in the spatial coordinates.
-%   A struct specifying the spatial dimensions as strings (that is dim1_str
-%   and dim2_str), and a string specifying the component of the B field(
-%   that is B_str)
+function [] = plotSurface(field, s)
+% Plot field component or magnitude as surface in a plane
+% input: field and struct containing plot information
 % Output: none
 
+% Generate data for plot
+data=getCompInPlane(field, s);
 % Desired spatial grid
 n=length(data(:,1));
 dp=1;
-g_dim1=(floor(min(data(:,1))):dp:ceil(max(data(:,1))));
-g_dim2=(floor(min(data(:,2))):dp:ceil(max(data(:,2))));
+d_dim1=(floor(min(data(:,1))):dp:ceil(max(data(:,1))));
+d_dim2=(floor(min(data(:,2))):dp:ceil(max(data(:,2))));
+[g_dim1, g_dim2]=meshgrid(d_dim1, d_dim2);
+% Make the surface using linear regression
 B=griddata(data(:,1), data(:,2), data(:,3), g_dim1, g_dim2);
-
-    
+% plot the surface
+figure(1)
+surf(g_dim1, g_dim2, B)
+% Scatter plot measurements
+hold on
+plot3(data(:,1), data(:,2), data(:,3), 'o')
+hold off
+xlabel(s.dim1)
+ylabel(s.dim2)
+zlabel(s.comp)
 end
 
