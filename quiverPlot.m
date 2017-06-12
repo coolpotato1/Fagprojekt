c8ca40b09e3bf990e7942e78ee0d273e08e337a7
Henning <henningtaufrederiksen@gmail.com>
2017-06-12 16:11:36 +0200

    Fikset quiverplot

--- quiverPlot.m
+++ quiverPlot.m
@@ -1,7 +1,9 @@
-function [  ] = quiverPlot( field)
+function [  ] = quiverPlot( field, s)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-quiver3(field(:,1), field(:,2), field(:,3), field(:,4), field(:,5), field(:,6), 3)
+M=calcMagnitude(field(:,4), field(:,5), field(:,6));
+field(M<(s.sort*max(M)),:)=[];
+quiver3(field(:,1), field(:,2), field(:,3), field(:,4), field(:,5), field(:,6))
 title('Quiverplot of the magnetic field in 3 dimensions')
 xlabel('x')
 ylabel('y')
